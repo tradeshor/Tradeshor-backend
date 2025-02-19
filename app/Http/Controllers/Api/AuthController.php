281e8ae7c5b8ceaf7ce97fc8263cd52db9c7568c
tradeshor <allisonmiller676@gmail.com>
2025-02-19 01:54:01 +0100

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Resources\UserTransformer;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = Auth::guard('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register(Request $request)
+    {
+      try {
+            $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|string|email|unique:users',
+                'password' => 'required|string|min:6',
+                'role' => 'required|in:admin,user'
+            ]);
+
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'role' => $request->role
+            ]);
+
+            return response()->json(['message' => 'User registered successfully'], 200);
+      } catch (\Throwable $th) {
+        throw new \Exception("Error Processing Request", 1);     
+        
+      }
+      
+    }
+
+    public function profile()
+    {
+        return response()->json(new UserTransformer(auth()->user()));
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => new UserTransformer(auth()->user())
+        ]);
+    }
+}
