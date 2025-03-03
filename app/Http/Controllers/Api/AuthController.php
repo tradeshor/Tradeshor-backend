71c495a8705d5160024cc55e0158323b0c3848a8
tradeshor <allisonmiller676@gmail.com>
2025-03-04 00:50:54 +0100

    created admin functions

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -35,6 +35,7 @@ class AuthController extends Controller
             ]);
 
 
+            
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
